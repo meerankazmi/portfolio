e14b5ff01275321ea72d4f8d8ee6ea69e76ff52d
meerankazmi <kazmimeeran@gmail.com>
2020-12-04 01:37:33 +0500

    Fixed the title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import "../styles/tailwind.css";
 import "../styles/main.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Portfolio by Kazmi</title>
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
